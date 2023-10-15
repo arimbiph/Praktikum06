6de2dcb7e2415528aba018ce08d40f3df8d8f06e
arimbiph <putriarimbi714@gmail.com>
2023-10-15 20:09:29 +0700

    nestedTernary Operator

--- Bilangan06.java
+++ Bilangan06.java
@@ -1,47 +1,20 @@
 import java.util.Scanner;
-public class Bilangan06 {public static void main(String[] args) {
-    Scanner sc = new Scanner (System.in);
+public class Bilangan06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    int bilangan1, bilangan2, bilangan3 ;
-    int bilTerbesar, total;
+        int bilangan1, bilangan2, bilangan3;
+        int bilTerbesar;
 
-    System.out.println("masukkan bilangan 1");
-    bilangan1 = sc.nextInt();
-    System.out.println("masukkan bilangan 2");
-    bilangan2 = sc.nextInt();
-    System.out.println("masukkan bilangan 3");
-    bilangan3 = sc.nextInt();
+        System.out.println("Masukkan bilangan 1: ");
+        bilangan1 = sc.nextInt();
+        System.out.println("Masukkan bilangan 2: ");
+        bilangan2 = sc.nextInt();
+        System.out.println("Masukkan bilangan 3: ");
+        bilangan3 = sc.nextInt();
 
-    bilTerbesar = bilangan1;
+        bilTerbesar = (bilangan1 > bilangan2) ? ((bilangan1 > bilangan3) ? bilangan1 : bilangan3) : ((bilangan2 > bilangan3) ? bilangan2 : bilangan3);
 
-    if (bilTerbesar < bilangan2) {
-        bilTerbesar=bilangan2;
-    }
-    if (bilTerbesar < bilangan3) {
-        bilTerbesar=bilangan3;
-    }
-    System.out.println(bilTerbesar);
+        System.out.println("Bilangan terbesar adalah: " + bilTerbesar);
 }
-
 }
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
